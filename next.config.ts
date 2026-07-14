d1748234435ab6b73bfa78c8b0a036983c4ee115
princerider7078-web <princerider7078-web@users.noreply.github.com>
2026-07-14 07:36:44 +0000

    859c44f0-14d9-4c67-a984-220f1dfc44ac

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
   images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
   reactStrictMode: false,
